6115a4051bd4c2d96f80ada79c67de0dafbb5d13
Jürno Ader <jyrno42@gmail.com>
2015-12-14 09:42:46 +0200

    PyPI build fixes 0.0.2

--- hammer/__init__.py
+++ hammer/__init__.py
@@ -1,6 +1,6 @@
 hammer_name = 'tg-hammer'
 hammer_description = 'Helpers for fabric based deployments.'
-hammer_version = '0.0.1'
+hammer_version = '0.0.2'
 
 __name__ = hammer_name
 __description__ = hammer_description
