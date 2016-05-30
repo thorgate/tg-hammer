47902203f804c8d3d8b8d87281d5ecabffaeed95
Rivo Laks <rivolaks@gmail.com>
2016-05-30 11:09:36 +0300

    Bump version to 0.1.0

--- hammer/__init__.py
+++ hammer/__init__.py
@@ -1,6 +1,6 @@
 hammer_name = 'tg-hammer'
 hammer_description = 'Helpers for fabric based deployments.'
-hammer_version = '0.0.5'
+hammer_version = '0.1.0'
 
 __name__ = hammer_name
 __description__ = hammer_description
