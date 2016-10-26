a201b60f80d12b5ac76d4eb39659b73a73ca3203
Jürno Ader <jyrno42@gmail.com>
2016-10-26 18:14:04 +0300

    Release v0.4.2

--- hammer/__init__.py
+++ hammer/__init__.py
@@ -1,6 +1,6 @@
 hammer_name = 'tg-hammer'
 hammer_description = 'Helpers for fabric based deployments.'
-hammer_version = '0.4.0'
+hammer_version = '0.4.2'
 
 __name__ = hammer_name
 __description__ = hammer_description
