224c34d69a03ed8b3e00123e99731f530ff591da
Jürno Ader <jyrno42@gmail.com>
2017-01-05 14:46:41 +0200

    Ensure run-tests.sh exits with correct code indicating wheter tests passed or not

--- docker-entrypoint-master.sh
+++ docker-entrypoint-master.sh
@@ -5,3 +5,7 @@ service ssh restart
 py.test
 # Uncomment the line below and comment the line above to leave the master running.
 # tail -f /dev/null
+
+# Ensure we set correct exitcode (CI)
+RES=$?
+exit $RES
--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-docker-compose up --build -d slave
-docker-compose up --build master
+# Build images
+docker-compose build
+
+# Bring slave up in detached mode
+docker-compose up -d slave
+
+# Run tests
+docker-compose run master /hammer/docker-entrypoint-master.sh
+RES=$?
+
+# Bring both containers down
 docker-compose down
+
+# Exit with the correct exitcode
+exit $RES
