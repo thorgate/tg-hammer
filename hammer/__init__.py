106c8309020eec57e14f5e2e09baed80e739a0c0
Jürno Ader <jyrno42@gmail.com>
2018-01-24 17:28:50 +0200

    Bump version

--- hammer/__init__.py
+++ hammer/__init__.py
@@ -1,6 +1,6 @@
 hammer_name = 'tg-hammer'
 hammer_description = 'Helpers for fabric based deployments.'
-hammer_version = '0.6.4'
+hammer_version = '0.6.5'
 
 __name__ = hammer_name
 __description__ = hammer_description
