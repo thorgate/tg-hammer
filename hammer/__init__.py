68c1aa3a94a677da89f1600ce9115584a3721422
Jyrno Ader <jyrno42@gmail.com>
2020-01-13 14:12:20 +0200

    Only run coveralls on py3.8\nEnsure __init__.py is valid on python3

--- hammer/__init__.py
+++ hammer/__init__.py
@@ -1,7 +1,9 @@
+import sys
+
 hammer_name = 'tg-hammer'
 hammer_description = 'Helpers for fabric based deployments.'
-hammer_version = '0.7.0a0'
+hammer_version = '0.7.0a1'
 
-__name__ = hammer_name
+__name__ = hammer_name if sys.version_info[0] < 3 else 'hammer'
 __description__ = hammer_description
 __version__ = hammer_version
