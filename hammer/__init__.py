911e094561bc4d82ccfabd1638ca2f901c5c91cf
Jyrno Ader <jyrno42@gmail.com>
2020-01-21 10:35:17 +0200

    Release rc0

--- hammer/__init__.py
+++ hammer/__init__.py
@@ -1,6 +1,6 @@
 hammer_name = 'tg-hammer'
 hammer_description = 'Helpers for fabric based deployments.'
-hammer_version = '0.7.0a3'
+hammer_version = '0.7.0rc0'
 
 __description__ = hammer_description
 __version__ = hammer_version
